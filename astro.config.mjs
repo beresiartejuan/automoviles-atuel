b510d6c28ecd9a3d366c5e8dd9e1c41452a18a29
juan beresiarte <juanberesiarte@gmail.com>
2025-11-24 16:31:06 -0300

    chore: update package dependencies and improve TypeScript configuration
    - Updated package name in package.json to an empty string.
    - Upgraded dependencies:
      - @astrojs/vercel from ^8.0.1 to ^9.0.1
      - @libsql/client from ^0.14.0 to ^0.15.15
      - astro from ^5.1.0 to ^5.16.0
      - bcrypt from ^5.1.1 to ^6.0.0
      - qr-code-styling from ^1.8.4 to ^1.9.2
      - uuid from ^11.0.3 to ^13.0.0
      - Added @types/uuid and valibot as new dependencies.
    - Updated devDependencies:
      - @types/bcrypt from ^5.0.2 to ^6.0.0
      - @types/jsonwebtoken from ^9.0.7 to ^9.0.10
    - Added new banner image and updated existing banner image.
    - Refined tsconfig.json paths to use relative paths for better clarity.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel";
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "http://automoviles-atuel.vercel.app",
-    vite: {
-        css: {
-            preprocessorOptions: {
-                sass: {
-                    api: "modern-compiler",
-                },
-                scss: {
-                    api: "modern-compiler",
-                },
-            },
-        },
-    },
-    output: "server",
-    adapter: vercel()
+  adapter: vercel(),
+  output: "server",
+  site: "http://localhost:1234",
 });
